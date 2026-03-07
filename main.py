8a948183d94fa3664a4385476f3087c98494f136
okachan45 <73508583+oka45@users.noreply.github.com>
2026-03-07 16:49:33 +0900

    feat: 天気を取得するAPIを追加 (#2)

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import requests
 
-st.set_page_config(page_title="Weekly Weather", page_icon="🌦️")
-st.title("Weekly Weather 🌦️")
-st.write("起動確認できたらOK！")
+def get_weather_api():
+    """
+    天気APIを使い最高気温・最低気温・天気状態を取得する関数
+    Returns:
+        dict or None: 正常に取得できた場合は dict、例外が発生した場合は None
+    """
+    url = 'https://api.open-meteo.com/v1/forecast'
+    params = {
+        "latitude": 35.6895,
+        "longitude": 139.6917,
+        "daily": ["temperature_2m_max", "temperature_2m_min", "weather_code"],
+        "timezone": "Asia/Tokyo",
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()  # 4xx/5xxなら例外
+    except requests.exceptions.RequestException:
+        st.error("天気情報の取得に失敗")
+        return None
+    return response.json()
+
+data = get_weather_api()
+
+if data is not None:
+    st.write(data)
